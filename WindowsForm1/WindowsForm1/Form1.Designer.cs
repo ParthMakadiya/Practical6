1314848f481c40d1412bf8709afd48db748ea8c8
ParthMakadiya <46336477+ParthMakadiya@users.noreply.github.com>
2019-03-30 11:15:50 +0530

    Add files via upload

---
+++ WindowsForm1/WindowsForm1/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace WindowsForm1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fname = new System.Windows.Forms.TextBox();
+            this.Middlename = new System.Windows.Forms.TextBox();
+            this.Lname = new System.Windows.Forms.TextBox();
+            this.Name = new System.Windows.Forms.Label();
+            this.Gender = new System.Windows.Forms.Label();
+            this.Male = new System.Windows.Forms.RadioButton();
+            this.Female = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fname
+            // 
+            this.fname.Location = new System.Drawing.Point(67, 27);
+            this.fname.Name = "fname";
+            this.fname.Size = new System.Drawing.Size(100, 20);
+            this.fname.TabIndex = 0;
+            // 
+            // Middlename
+            // 
+            this.Middlename.Location = new System.Drawing.Point(199, 27);
+            this.Middlename.Name = "Middlename";
+            this.Middlename.Size = new System.Drawing.Size(100, 20);
+            this.Middlename.TabIndex = 1;
+            // 
+            // Lname
+            // 
+            this.Lname.Location = new System.Drawing.Point(334, 27);
+            this.Lname.Name = "Lname";
+            this.Lname.Size = new System.Drawing.Size(100, 20);
+            this.Lname.TabIndex = 2;
+            // 
+            // Name
+            // 
+            this.Name.AutoSize = true;
+            this.Name.Location = new System.Drawing.Point(13, 33);
+            this.Name.Name = "Name";
+            this.Name.Size = new System.Drawing.Size(35, 13);
+            this.Name.TabIndex = 3;
+            this.Name.Text = "Name";
+            // 
+            // Gender
+            // 
+            this.Gender.AutoSize = true;
+            this.Gender.Location = new System.Drawing.Point(16, 79);
+            this.Gender.Name = "Gender";
+            this.Gender.Size = new System.Drawing.Size(42, 13);
+            this.Gender.TabIndex = 4;
+            this.Gender.Text = "Gender";
+            // 
+            // Male
+            // 
+            this.Male.AutoSize = true;
+            this.Male.Location = new System.Drawing.Point(67, 77);
+            this.Male.Name = "Male";
+            this.Male.Size = new System.Drawing.Size(48, 17);
+            this.Male.TabIndex = 5;
+            this.Male.TabStop = true;
+            this.Male.Text = "Male";
+            this.Male.UseVisualStyleBackColor = true;
+            this.Male.CheckedChanged += new System.EventHandler(this.Male_CheckedChanged);
+            // 
+            // Female
+            // 
+            this.Female.AutoSize = true;
+            this.Female.Location = new System.Drawing.Point(135, 77);
+            this.Female.Name = "Female";
+            this.Female.Size = new System.Drawing.Size(59, 17);
+            this.Female.TabIndex = 6;
+            this.Female.TabStop = true;
+            this.Female.Text = "Female";
+            this.Female.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Date";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(67, 120);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(349, 297);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(450, 297);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "reset";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(502, 27);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(100, 106);
+            this.pictureBox.TabIndex = 11;
+            this.pictureBox.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(502, 171);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Upload";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(729, 479);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Female);
+            this.Controls.Add(this.Male);
+            this.Controls.Add(this.Gender);
+            this.Controls.Add(this.Name);
+            this.Controls.Add(this.Lname);
+            this.Controls.Add(this.Middlename);
+            this.Controls.Add(this.fname);
+            //this.Name = "Form1";
+            this.Text = "StudentRegistrationForm";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox fname;
+        private System.Windows.Forms.TextBox Middlename;
+        private System.Windows.Forms.TextBox Lname;
+        private System.Windows.Forms.Label Name;
+        private System.Windows.Forms.Label Gender;
+        private System.Windows.Forms.RadioButton Male;
+        private System.Windows.Forms.RadioButton Female;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ WindowsForm1/WindowsForm1/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace WindowsForm1
+{
+    public partial class Form1 : Form
+    {
+        string imgPath; public String gender;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String source = @"Data Source=CE3COMP3\sqlexpress;Initial Catalog=DBstudent;Integrated Security=True;Pooling=False";
+            SqlConnection con = new SqlConnection(source);
+            con.Open();
+            String ins = "insert into Tbl1(fname,Middlename,Lname,gender,Date) values('"+fname.Text+"','"+ Middlename.Text+ "','" + Lname.Text + "','" +gender+"','"+ dateTimePicker1.Value.Date +"')"; 
+            SqlCommand sc = new SqlCommand(ins, con);
+            
+            int i=sc.ExecuteNonQuery();
+            if (i > -1)
+            {
+                MessageBox.Show("Entered into database");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Png|*.png";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                imgPath = @"C:\Users\CRP\Desktop\Images\"+ openFileDialog1.SafeFileName;
+               pictureBox.Image = Image.FromFile(openFileDialog1.FileName);
+            }
+        }
+
+        private void Male_CheckedChanged(object sender, EventArgs e)
+        {
+            
+            if (Male.Checked)
+            {
+                gender = "Male";
+
+            }
+            else
+            {
+                gender = "Female";
+            }
+        }
+    }
+}
